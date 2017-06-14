05c7d6b7c62e6e3b5cafba77293a4784a18a9c4e
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-14 18:15:29 +0100

    Add mongodb mechanism

--- start.js
+++ start.js
@@ -2,3 +2,10 @@ const mongoose = require('mongoose')
 
 // import environment variables
 require('dotenv').config({ path: 'variables.env' })
+
+// connect to mongodb and handle bad connection
+mongoose.connect(process.env.DATABASE)
+mongoose.Promise = global.Promise
+mongoose.connection.on('error', (err) => {
+  console.error(`💔💔💔💔💔💔 ➡ ${err.message}`)
+})
