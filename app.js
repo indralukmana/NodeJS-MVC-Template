fff50fae3ffa6c33d21e1c7c57e085072007eaa9
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-14 18:30:27 +0100

    define static path from public

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 const express = require('express')
 const session = require('express-session')
 
+// import path for working with directory and files
+const path = require('path')
+
 // create the Express app
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname), 'views') // the folder for template files
 app.set('view engine', 'pug')
+
+// serve static file in public folder
+app.use(express.static(path.join(__dirname, 'public')))
