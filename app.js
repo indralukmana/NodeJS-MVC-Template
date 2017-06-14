97d80129eff8f6a54db30742e753032cc3781dad
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-14 18:24:29 +0100

    setup view engine

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const session = require('express-session')
 
 // create the Express app
 const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname), 'views')
+app.set('view engine', 'pug')
