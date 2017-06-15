5e7d10a715c503a2497db100e80811cebc4c39c3
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-15 15:51:25 +0100

    add the app on start mechanism

--- start.js
+++ start.js
@@ -9,3 +9,10 @@ mongoose.Promise = global.Promise
 mongoose.connection.on('error', (err) => {
   console.error(`💔💔💔💔💔💔 ➡ ${err.message}`)
 })
+
+// start the app
+const app = require('./app')
+app.set('port', process.env.PORT || 7777)
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express is running on PORT ${server.address().port}`)
+})
