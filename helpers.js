66de222449f7760a8c7b14abd03c421cd6aa08d5
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-15 12:47:33 +0100

    created a file for helpers function

---
+++ helpers.js
@@ -0,0 +1,8 @@
+// import library for reading files on the system
+const fs = require('fs')
+
+// 'Dump' function for debuggin data
+exports.dump = (obj) => JSON.stringify(obj, null, 2)
+
+// Site name details
+exports.siteName = 'MVC Template'
