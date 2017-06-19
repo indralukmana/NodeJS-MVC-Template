5c5867174e6f994b55699651dc01029a28bf3230
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-19 11:09:45 +0100

    add testing layout

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('MVC Template')
+  res.render('layout')
 })
 
 module.exports = router
